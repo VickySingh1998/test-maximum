dbf3f125abc150ec57efd881a17d00963630742f
vickysingh1998 <vickysingh8697@gmail.com>
2022-08-01 00:46:37 +0530

    refactor: to create Generic

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,24 +1,32 @@
 package com.bridgelabz;
 
-public class Maximum {
-    public static <X extends Comparable>X maximum(X a, X b, X c) {
+public class Maximum <X extends Comparable> {
+    X a, b, c;
+
+    public Maximum(X a, X b, X c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public X testMaximum() {
+
         X max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
         if (c.compareTo(max) > 0) {
-            max=c;
+            max = c;
         }
         return max;
     }
 
     public static void main(String[] args) {
-        Integer resultInt = maximum(67, 76, 24);
-        Float resultFloat = maximum((float)23.3, (float) 45.7, (float) 28.9);
-        String resultString = maximum("Vicky", "Raja", "Deep");
-        System.out.println("The Maximum Integer Number Is : " +resultInt);
-        System.out.println("The Maximum Float Number Is : " +resultFloat);
-        System.out.println("The Maximum String Is : " +resultString);
+        Maximum<Integer> resultInt = new Maximum<>(67, 76, 24);
+        Maximum<Float> resultFloat = new Maximum<>(23.3f, 45.7f, 28.9f);
+        Maximum<String> resultString = new Maximum<>("Vicky", "Raja", "Deep");
+        System.out.println("The Maximum Integer Number Is : " +resultInt.testMaximum());
+        System.out.println("The Maximum Float Number Is : " +resultFloat.testMaximum());
+        System.out.println("The Maximum String Is : " +resultString.testMaximum());
     }
 }
 

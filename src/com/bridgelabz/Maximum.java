8a30cc00e0d91e027e28d5d9e190e02b7b94374e
vickysingh1998 <vickysingh8697@gmail.com>
2022-07-31 23:52:36 +0530

    modified: to find the maximum integer and float number

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Maximum {
     public static Integer maximumInteger(Integer number1, Integer number2, Integer number3) {
@@ -13,16 +13,23 @@ public class Maximum {
         }
         return max;
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter First Number : ");
-        int number1 = scanner.nextInt();
-        System.out.println("Enter Second Number : ");
-        int number2 = scanner.nextInt();
-        System.out.println("Enter Third Number : ");
-        int number3 = scanner.nextInt();
 
-        System.out.println(" The Maximum Integer Is : " +maximumInteger(number1, number2, number3));
+    public static Float maximumFloat(Float number1, Float number2, Float number3) {
+        Float max = number1;
+        if (number2.compareTo(max) > 0) {
+            max = number2;
+        }
+        if (number3.compareTo(max) > 0) {
+            max = number3;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Integer resultInt =  maximumInteger(67, 76, 24);
+        Float resultFloat = maximumFloat((float)23.3, (float) 45.7, (float) 28.9);
+        System.out.println("The Maximum Integer Number Is : " +resultInt);
+        System.out.println("The Maximum Float Number Is : " +resultFloat);
     }
 }
 

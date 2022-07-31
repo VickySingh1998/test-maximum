3e7785480a6e1cf55ee5bc56ea0fab73b7c56992
vickysingh1998 <vickysingh8697@gmail.com>
2022-08-01 00:48:57 +0530

    refactor: to create generic class

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -23,7 +23,7 @@ public class Maximum <X extends Comparable> {
     public static void main(String[] args) {
         Maximum<Integer> resultInt = new Maximum<>(67, 76, 24);
         Maximum<Float> resultFloat = new Maximum<>(23.3f, 45.7f, 28.9f);
-        Maximum<String> resultString = new Maximum<>("Vicky", "Raja", "Deep");
+        Maximum<String> resultString = new Maximum<>("Vicky", "Raja", "Shane");
         System.out.println("The Maximum Integer Number Is : " +resultInt.testMaximum());
         System.out.println("The Maximum Float Number Is : " +resultFloat.testMaximum());
         System.out.println("The Maximum String Is : " +resultString.testMaximum());

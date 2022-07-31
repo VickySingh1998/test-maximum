9ef98c8295af9c3b050d840f1e13943d6600556b
vickysingh1998 <vickysingh8697@gmail.com>
2022-08-01 00:14:20 +0530

    modified: to find the maximum integer and float

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -25,24 +25,11 @@ public class Maximum {
         return max;
     }
 
-    public static String maximumString(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
     public static void main(String[] args) {
         Integer resultInt =  maximumInteger(67, 76, 24);
         Float resultFloat = maximumFloat((float)23.3, (float) 45.7, (float) 28.9);
-        String resultString = maximumString("Vicky", "Raja", "Deep");
         System.out.println("The Maximum Integer Number Is : " +resultInt);
         System.out.println("The Maximum Float Number Is : " +resultFloat);
-        System.out.println("The Maximum String Is : " +resultString);
     }
 }
 
